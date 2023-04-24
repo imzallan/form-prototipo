1614cfd7e1c355daeff841a25e36f9436c2cbffc
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-24 17:57:57 -0300

    feat: edit label field

--- src/FormBuilder.js
+++ src/FormBuilder.js
@@ -3,6 +3,13 @@ import { Form, Dropdown, DropdownButton, Button, Container, Row, Col, FormContro
 
 const FormBuilder = () => {
   const [formFields, setFormFields] = useState([]);
+  const [labelFieldEdit, setLabelFieldEdit] = useState(false);
+
+  const labelFieldEditHandleChange = (index) => (e) => {
+    setLabelFieldEdit({
+      id: index,
+    });
+  }
 
   const handleAddField = (fieldType) => {
     switch (fieldType) {
@@ -24,6 +31,11 @@ const FormBuilder = () => {
     setFormFields(formFields.filter((field, i) => i !== index));
   };
 
+  const handleEditField = (currentFieldIndex) => {
+    const fieldSelectedIndex = formFields.findIndex((field, index) => index === currentFieldIndex);
+    formFields[fieldSelectedIndex].label = document.getElementById("formLabel").value;
+  }
+
   return (
     <Container className="my-5">
       <Row className="justify-content-center">
@@ -40,7 +52,34 @@ const FormBuilder = () => {
             </DropdownButton>
             {formFields.map((field, index) => (
               <Form.Group key={index} className="my-3">
-                <Form.Label>{field.label}</Form.Label>
+                {
+                  labelFieldEdit?.id === index ? (
+                    <Form.Group as={Col} className="mb-3" controlId="formLabel">
+                      <Form.Label column>
+                        Edit the title field
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control type="text" defaultValue={field.label} />
+                      </Col>
+                      <Button className="ml-3" variant="danger" onClick={labelFieldEditHandleChange(false)}>
+                        Cancel
+                      </Button>
+                      <Button className="ml-3" variant="success" onClick={() => {
+                        handleEditField(index);
+                        return setLabelFieldEdit(false)
+                      }}>
+                        Add new title
+                      </Button>
+                    </Form.Group>
+                  ) : (
+                    <>
+                      <Form.Label>{field.label}</Form.Label>
+                      <Button className="ml-3" variant="primary" onClick={labelFieldEditHandleChange(index)}>
+                        Edit
+                      </Button>
+                    </>
+                  )
+                }
                 {field.type === 'text' && <FormControl type="text" placeholder="Enter text" />}
                 {field.type === 'number' && <FormControl type="number" placeholder="Enter number" />}
                 {field.type === 'file' && <FormControl type="file" />}
