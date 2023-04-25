e4f90ce0f0ef76645a83d7a28b7c6dfdf0b6a4c3
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-25 14:49:30 -0300

    refactor: Form Builder Body Inputs

--- src/components/FormBuilder.js
+++ src/components/FormBuilder.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col, FormControl } from 'react-bootstrap';
 import { FormBuilderHeader } from './FormBuilder/Header';
+import { FormBuilderBody } from './FormBuilder/Body';
 
 const FormBuilder = () => {
   const [formFields, setFormFields] = useState([]);
-
-  const handleRemoveField = (index) => {
-    setFormFields(formFields.filter((field, i) => i !== index));
-  };
 
   return (
     <Container className="my-5">
@@ -15,17 +12,7 @@ const FormBuilder = () => {
         <Col md={8}>
           <Form>
             <FormBuilderHeader formFields={formFields} setFormFields={setFormFields} />
-            {formFields.map((field, index) => (
-              <Form.Group key={index} className="my-3">
-                <Form.Label>{field.label}</Form.Label>
-                {field.type === 'text' && <FormControl type="text" placeholder="Enter text" />}
-                {field.type === 'number' && <FormControl type="number" placeholder="Enter number" />}
-                {field.type === 'file' && <FormControl type="file" />}
-                <Button className="ml-3" variant="danger" onClick={() => handleRemoveField(index)}>
-                  Remove
-                </Button>
-              </Form.Group>
-            ))}
+            <FormBuilderBody formFields={formFields} setFormFields={setFormFields} />
           </Form>
         </Col>
       </Row>
