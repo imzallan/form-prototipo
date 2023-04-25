0f99fe4485399bbb8a30ff16371bbfcc382940c4
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-25 14:42:02 -0300

    refactor: Form Builder Dropdown Header

--- src/components/FormBuilder.js
+++ src/components/FormBuilder.js
@@ -1,24 +1,9 @@
 import React, { useState } from 'react';
-import { Form, Dropdown, DropdownButton, Button, Container, Row, Col, FormControl } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col, FormControl } from 'react-bootstrap';
+import { FormBuilderHeader } from './FormBuilder/Header';
 
 const FormBuilder = () => {
   const [formFields, setFormFields] = useState([]);
-
-  const handleAddField = (fieldType) => {
-    switch (fieldType) {
-      case 'Text Field':
-        setFormFields([...formFields, { type: 'text', label: 'Text Field' }]);
-        break;
-      case 'Number Field':
-        setFormFields([...formFields, { type: 'number', label: 'Number Field' }]);
-        break;
-      case 'File Upload Field':
-        setFormFields([...formFields, { type: 'file', label: 'File Upload Field' }]);
-        break;
-      default:
-        break;
-    }
-  };
 
   const handleRemoveField = (index) => {
     setFormFields(formFields.filter((field, i) => i !== index));
@@ -29,15 +14,7 @@ const FormBuilder = () => {
       <Row className="justify-content-center">
         <Col md={8}>
           <Form>
-            <DropdownButton
-              id="dropdown-basic-button"
-              title="Add Field"
-              className="mb-3"
-            >
-              <Dropdown.Item onClick={() => handleAddField('Text Field')}>Text Field</Dropdown.Item>
-              <Dropdown.Item onClick={() => handleAddField('Number Field')}>Number Field</Dropdown.Item>
-              <Dropdown.Item onClick={() => handleAddField('File Upload Field')}>File Upload Field</Dropdown.Item>
-            </DropdownButton>
+            <FormBuilderHeader formFields={formFields} setFormFields={setFormFields} />
             {formFields.map((field, index) => (
               <Form.Group key={index} className="my-3">
                 <Form.Label>{field.label}</Form.Label>
