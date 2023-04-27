1b661087e2dd33e296e547b708149161009dd01c
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-27 13:50:25 -0300

    fix: controll id's in the add button form component

--- src/components/FormActions/Buttons/AddForm.js
+++ src/components/FormActions/Buttons/AddForm.js
@@ -1,10 +1,14 @@
 import { Button } from "reactstrap";
 import FormBuilder from "../../FormBuilder";
 
-export const ButtonAddForm = ({ forms, setForms }) => {
+export const ButtonAddForm = ({
+  forms,
+  setForms
+}) => {
+  const lastId = forms.length > 0 ? forms[forms.length - 1].id : 0;
   const handleAddForm = () => {
     setForms((currentForms) => {
-      const id = currentForms.length > 0 ? currentForms.length + 1 : 1;
+      const id = currentForms.length > 0 ? lastId + 1 : 1;
       return [
         ...currentForms,
         {
