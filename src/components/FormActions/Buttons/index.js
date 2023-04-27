0bea26692844ba46a12ccb5e372454baebf27bbd
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-27 13:52:20 -0300

    feat: FormActions implements in index file in component

---
+++ src/components/FormActions/Buttons/index.js
@@ -0,0 +1,2 @@
+export { ButtonAddForm } from "./AddForm";
+export { ButtonDeleteForm } from "./DeleteForm";
--- src/components/FormActions/index.js
+++ src/components/FormActions/index.js
@@ -1,30 +1,33 @@
-import {
-  Button,
-  Pagination,
-  PaginationItem,
-  PaginationLink
-} from "reactstrap";
 import { Card } from "react-bootstrap";
 
-import { ButtonAddForm } from "./Buttons/AddForm";
+import { ButtonAddForm, ButtonDeleteForm } from "./Buttons";
+import { FormPagination } from "./Pagination";
 
 export const FormActions = ({
   forms,
-  activePage,
-  handlePageChange,
-  handleAddForm,
-  handleRemoveForm,
   setForms,
+  activePage,
+  setActivePage
 }) => {
+
   return (
     <Card.Footer>
       <div className="d-flex justify-content-between align-items-center">
-        <ButtonAddForm forms={forms} setForms={setForms} />
-        {/* <Button onClick={handleAddForm}>Add Form</Button> */}
-        <Pagination>
-          {forms.map((form, index) => (<PaginationItem key={form.id} active={index + 1 === activePage}><PaginationLink onClick={() => handlePageChange(index + 1)}>{index + 1}</PaginationLink></PaginationItem>))}
-        </Pagination>
-        {forms.length > 1 && (<Button onClick={() => handleRemoveForm(activePage - 1)}>Remove Form</Button>)}
+        <ButtonAddForm
+          forms={forms}
+          setForms={setForms}
+        />
+        <FormPagination
+          forms={forms}
+          activePage={activePage}
+          setActivePage={setActivePage}
+        />
+        <ButtonDeleteForm
+          forms={forms}
+          setForms={setForms}
+          activePage={activePage}
+          setActivePage={setActivePage}
+        />
       </div>
     </Card.Footer>
   );
